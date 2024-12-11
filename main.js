9d5a5b94750342db7400fbee804cf6098bfd1dc6
Utkarsh <104618865+utkarshkrr@users.noreply.github.com>
2024-12-11 23:25:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+//javascript for the page
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+        
+$(window).on("load", function () {
+    $(".loader-container").fadeOut("slow");
+});
+
+function toggleTheme() {
+    $('*').toggleClass('dark-mode');
+    // Toggle dark mode preference
+    if ($('*').hasClass('dark-mode')) {
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        localStorage.removeItem('darkMode');
+    }
+}
+// Check if dark mode preference is saved and apply it
+if (localStorage.getItem('darkMode') === 'enabled') {
+    $('*').addClass('dark-mode');
+}
+
+document.onreadystatechange = function () {
+    let progressBar = document.getElementById('progress-bar');
+    if (document.readyState === 'interactive') {
+        progressBar.style.width = '75%'; // Midway point
+    } else if (document.readyState === 'complete') {
+        progressBar.style.width = '100%'; // Fully loaded
+        setTimeout(function () {
+            document.getElementById('progress-bar-container').style.display = 'none'; // Hide the progress bar
+        }, 1000); // Delay to allow the user to see the fully loaded state
+    }
+};
