2d8f47d6ff5cce835dd7a58c60d4c4ab5a9fb18e
Utkarsh <104618865+utkarshkrr@users.noreply.github.com>
2025-07-13 12:01:40 +0545

    Update script.js
    sidepanel improvement

--- script.js
+++ script.js
@@ -3,37 +3,47 @@ var tabContents = document.getElementsByClassName("tab_contents");
 
 // Tab switching with smooth transition
 function openTab(tabName) {
-    var tabLinks = document.getElementsByClassName('tab_links');
-    var tabContents = document.getElementsByClassName('tab_contents');
-
     for (var i = 0; i < tabLinks.length; i++) {
         tabLinks[i].classList.remove('activeLink');
     }
 
     for (var i = 0; i < tabContents.length; i++) {
         tabContents[i].classList.remove('activeTab');
-        tabContents[i].style.display = "none"; // Hide all tabs
+        tabContents[i].style.display = "none";
     }
 
     event.currentTarget.classList.add('activeLink');
 
     var currentTab = document.getElementById(tabName);
-    currentTab.style.display = "block"; // Show the clicked tab
+    currentTab.style.display = "block";
 
     setTimeout(function () {
-        currentTab.classList.add('activeTab'); // Trigger smooth transition
+        currentTab.classList.add('activeTab');
     }, 10);
 }
 
+// Mobile menu open/close
 var sideMenu = document.getElementById("sideMenu");
 
 function openMenu() {
     sideMenu.style.right = "0";
 }
+
 function closeMenu() {
     sideMenu.style.right = "-200px";
 }
 
+// Close menu when clicking outside
+document.addEventListener('click', function (event) {
+    const isClickInsideMenu = sideMenu.contains(event.target);
+    const isHamburger = event.target.closest('.fa-bars');
+
+    if (!isClickInsideMenu && !isHamburger) {
+        closeMenu();
+    }
+});
+
+// Contact form submission
 const scriptURL = 'https://script.google.com/macros/s/AKfycbx4_0LBjWRtdgfIu7vGkJoUOi0uyWdpFecYaCGo3lBCWgX4qZYkxZkVlN33TrA5rLE6/exec';
 const form = document.forms['submit-to-google-sheet'];
 const msg = document.getElementById("msg");
